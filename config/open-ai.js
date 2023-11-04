a4fa411464e45f971c0953e0b735e54383cf5226
Lovedeep <91473234+LovedeepSingh19@users.noreply.github.com>
2023-11-05 00:39:00 +0530

    final-test-2

--- config/open-ai.js
+++ config/open-ai.js
@@ -1,7 +1,9 @@
 import { Configuration, OpenAIApi } from 'openai';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
